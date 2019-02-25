db383e06fd2552b6a52df85d078e17ef800507f6
Michael Stenta <mike@mstenta.net>
2019-02-25 11:50:12 -0500

    Initial Flask application.

---
+++ farmOSaggregator/__init__.py
@@ -0,0 +1,5 @@
+# Import Flask.
+from flask import Flask
+
+# Create a Flask application.
+app = Flask(__name__)
---
+++ setup.py
@@ -0,0 +1,24 @@
+import io
+
+from setuptools import find_packages, setup
+
+with io.open('README.md', 'rt', encoding='utf8') as f:
+    readme = f.read()
+
+setup(
+    name='farmOSaggregator',
+    version='1.0.0',
+    url='https://github.com/farmOS/farmOS-aggregator',
+    license='GPLv3',
+    maintainer='Michael Stenta',
+    maintainer_email='mike@mstenta.net',
+    description='An application for aggregating data from multiple farmOS instances.',
+    long_description=readme,
+    long_description_content_type='text/markdown',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'flask',
+    ],
+)
