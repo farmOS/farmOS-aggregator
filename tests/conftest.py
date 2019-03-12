f6fd2a055f7893ced553820b69d44ab1cc583f6d
Paul Weidner <paul.weidner@gmail.com>
2019-03-11 20:38:32 -0700

    Add valid authentication test

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,11 +3,25 @@ pytest configuration file.
 
 Includes fixtures for unit and functional tests
 """
- 
+
 import pytest
+
+from farmOSaggregator import farmOSaggregator, db
 from farmOSaggregator.models import Farm
- 
+
 @pytest.fixture(scope='module')
 def new_farm():
     farm = Farm('test.url', 'test name', 'testusername', 'testpass')
     return farm
+
+@pytest.fixture(scope='module')
+def client():
+    farmOSaggregator.app.config['TESTING'] = True
+    client = farmOSaggregator.app.test_client()
+
+    ctx = farmOSaggregator.app.app_context()
+    ctx.push()
+
+    yield client
+
+    ctx.pop()
---
+++ tests/functional/test_auth.py
@@ -0,0 +1,23 @@
+""" This file (test_auth.py) tests the basic authentication of the client
+
+"""
+import base64
+from farmOSaggregator import farmOSaggregator
+
+def login(client, username, password):
+    encoded_credentials = base64.b64encode(f'{username}:{password}'.encode()).decode('utf-8')
+    headers = {'Authorization': 'Basic ' + encoded_credentials }
+    #return client.get("/", headers=headers)
+    return client.get("/", headers=headers)
+
+def test_valid_login(client):
+    """
+    GIVEN a Flask app
+    WHEN the '/' page is requested with valid credentials
+    THEN check the page is loaded
+    """
+    print(farmOSaggregator.app.config['BASIC_AUTH_USERNAME'])
+    response = login(client, farmOSaggregator.app.config['BASIC_AUTH_USERNAME'], farmOSaggregator.app.config['BASIC_AUTH_PASSWORD'])
+
+    assert response.status_code == 200
+    assert b'There are no items in the table.' in response.data
