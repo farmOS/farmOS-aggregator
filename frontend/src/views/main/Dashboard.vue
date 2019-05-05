e39965a8e4aab091bd6b0f0e6fc21e0da9464160
Paul Weidner <paul.weidner@gmail.com>
2019-05-05 13:59:48 -0700

    Improve dashboard, add Farm buttons, link to farmOS, closes #13

--- frontend/src/views/main/Dashboard.vue
+++ frontend/src/views/main/Dashboard.vue
@@ -5,13 +5,75 @@
         <div class="headline primary--text">Dashboard</div>
       </v-card-title>
       <v-card-text>
-        <div class="headline font-weight-light ma-5">Welcome {{greetedUser}}</div>
+        <div class="headline ma-3">Welcome {{greetedUser}}</div>
+        <div class="body2 font-weight ">
+          <p>
+            This is the dashboard page of farmOS-aggregator. Use the menu on the left
+            or some of the options below to view and manage farmOS profiles.
+          </p>
+        </div>
       </v-card-text>
-      <v-card-actions>
-        <v-btn to="/main/profile/view">View Profile</v-btn>
-        <v-btn to="/main/profile/edit">Edit Profile</v-btn>
-        <v-btn to="/main/profile/password">Change Password</v-btn>
-      </v-card-actions>
+      <v-card-text>
+      <ul>
+        <li>
+          <a href="https://farmos.org/">farmOS.org</a>
+        </li>
+        <li>
+          farmOS-aggregator documentation
+        </li>
+        <li>
+          <a href="https://github.com/farmOS/farmOS-aggregator">farmOS-aggregator Github</a>
+        </li>
+      </ul>
+
+
+      </v-card-text>
+    </v-card>
+
+    <v-card class="ma-3 pa-3">
+      <v-card-title primary-title>
+        <div class="headline primary--text">Farm Profiles</div>
+      </v-card-title>
+
+      <v-card-text>
+        <div class="subheading">Manage farmOS profiles</div>
+        <v-card-actions>
+          <v-btn color="info" to="/main/farm/farms/">
+            Manage Farms
+            <v-icon right dark>list</v-icon>
+          </v-btn>
+          <v-btn color="success" to="/main/farm/farms/add">
+            Add Farm
+            <v-icon right dark>add</v-icon>
+          </v-btn>
+        </v-card-actions>
+      </v-card-text>
+
+    </v-card>
+
+    <v-card class="ma-3 pa-3">
+      <v-card-title primary-title>
+        <div class="headline primary--text">User Profiles</div>
+      </v-card-title>
+
+      <v-card-text>
+        <div class="subheading">Manage user profiles</div>
+        <v-card-actions>
+          <v-btn to="/main/profile/view">
+            View Profile
+            <v-icon right dark>info</v-icon>
+          </v-btn>
+          <v-btn to="/main/profile/edit">
+            Edit Profile
+            <v-icon right dark>edit</v-icon>
+          </v-btn>
+          <v-btn to="/main/profile/password">
+            Change Password
+            <v-icon right dark>edit</v-icon>
+          </v-btn>
+        </v-card-actions>
+      </v-card-text>
+
     </v-card>
   </v-container>
 </template>
