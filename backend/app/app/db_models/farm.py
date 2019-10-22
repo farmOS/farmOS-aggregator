efafcd56b2a4333fc0f17a2ad773c19745a6693d
Paul Weidner <paul.weidner@gmail.com>
2019-10-22 11:18:33 -0700

    Make the farm URL unique. Fixes #23.

--- backend/app/app/db_models/farm.py
+++ backend/app/app/db_models/farm.py
@@ -6,6 +6,6 @@ from app.db.base_class import Base
 class Farm(Base):
     id = Column(Integer, primary_key=True, index=True)
     farm_name = Column(String, index=True)
-    url = Column(String, index=True)
+    url = Column(String, index=True, unique=True)
     username = Column(String, index=True)
     password = Column(String, index=True)
