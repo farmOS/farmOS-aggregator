017385fa7feb4fa037d3c9b631ae313fe45b3b0c
Paul Weidner <paul.weidner@gmail.com>
2019-10-22 11:11:44 -0700

    Remove the "item" placeholder object. Fixes #25.

--- backend/app/app/db_models/user.py
+++ backend/app/app/db_models/user.py
@@ -11,4 +11,3 @@ class User(Base):
     hashed_password = Column(String)
     is_active = Column(Boolean(), default=True)
     is_superuser = Column(Boolean(), default=False)
-    items = relationship("Item", back_populates="owner")
