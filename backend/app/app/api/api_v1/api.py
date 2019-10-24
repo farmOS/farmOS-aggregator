0a378449d7ab228e9dd55789c7c3ffe9fef58486
Paul Weidner <paul.weidner@gmail.com>
2019-10-24 16:38:05 -0700

    Add OAuth as a dependency for all API Endpoints. Closes #35.

--- backend/app/app/api/api_v1/api.py
+++ backend/app/app/api/api_v1/api.py
@@ -1,15 +1,50 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
 
 from app.api.api_v1.endpoints import login, users, utils
 from app.api.api_v1.endpoints.farms import farms, logs, assets, terms, areas
+from app.api.utils.security import get_current_user
 
 api_router = APIRouter()
 api_router.include_router(login.router, tags=["login"])
 api_router.include_router(users.router, prefix="/users", tags=["users"])
 api_router.include_router(utils.router, prefix="/utils", tags=["utils"])
 
-api_router.include_router(farms.router,  prefix="/farms", tags=["farms"])
-api_router.include_router(logs.router, prefix="/farms/logs", tags=["farm logs"])
-api_router.include_router(assets.router, prefix="/farms/assets", tags=["farm assets"])
-api_router.include_router(terms.router, prefix="/farms/terms", tags=["farm terms"])
-api_router.include_router(areas.router, prefix="/farms/areas", tags=["farm areas"])
+# Include /farms endpoints.
+api_router.include_router(
+    farms.router,
+    prefix="/farms",
+    tags=["farms"],
+    dependencies=[Depends(get_current_user)]
+)
+
+# Include /farms/logs endpoints.
+api_router.include_router(
+    logs.router,
+    prefix="/farms/logs",
+    tags=["farm logs"],
+    dependencies=[Depends(get_current_user)]
+)
+
+# Include /farms/assets endpoints.
+api_router.include_router(
+    assets.router,
+    prefix="/farms/assets",
+    tags=["farm assets"],
+    dependencies=[Depends(get_current_user)]
+)
+
+# Include /farms/terms endpoints.
+api_router.include_router(
+    terms.router,
+    prefix="/farms/terms",
+    tags=["farm terms"],
+    dependencies=[Depends(get_current_user)]
+)
+
+# Include /farms/areas endpoints.
+api_router.include_router(
+    areas.router,
+    prefix="/farms/areas",
+    tags=["farm areas"],
+    dependencies=[Depends(get_current_user)]
+)
