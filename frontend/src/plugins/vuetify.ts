e0669cc3ece039d3e43d9cb5daabba5ab34c549a
Paul Weidner <paul.weidner@gmail.com>
2019-10-24 15:30:14 -0700

    Fix frontend build errors. Closes #36.

--- frontend/src/plugins/vuetify.ts
+++ frontend/src/plugins/vuetify.ts
@@ -1,8 +1,5 @@
 import Vue from 'vue';
 import Vuetify from 'vuetify';
-import 'vuetify/dist/vuetify.min.css';
-import '@mdi/font/css/materialdesignicons.css' // Ensure you are using css-loader
-
 
 Vue.use(Vuetify);
 
@@ -14,15 +11,15 @@ export default new Vuetify({
     dark: false,
     themes: {
       light: {
-        primary: '#336633', //farmos-green-dark: #336633;
-        secondary: '#4e8b31', //farmos-green: #4e8b31;
-        accent: '#60af32', //farmos-green-light: #60af32;
+        primary: '#336633', // farmos-green-dark: #336633;
+        secondary: '#4e8b31', // farmos-green: #4e8b31;
+        accent: '#60af32', // farmos-green-light: #60af32;
       },
       dark: {
-        primary: '#336633', //farmos-green-dark: #336633;
-        secondary: '#4e8b31', //farmos-green: #4e8b31;
-        accent: '#60af32', //farmos-green-light: #60af32;
-      }
-    }
-  }
+        primary: '#336633', // farmos-green-dark: #336633;
+        secondary: '#4e8b31', // farmos-green: #4e8b31;
+        accent: '#60af32', // farmos-green-light: #60af32;
+      },
+    },
+  },
 });
