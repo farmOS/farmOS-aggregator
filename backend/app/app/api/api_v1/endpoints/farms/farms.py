a48d0c2675de8f35f82aeb0ef3fff1b70453a0bb
Paul Weidner <paul.weidner@gmail.com>
2019-10-30 10:04:31 -0600

    Add Farm API endpoint to Authorize a farm via the OAuth Authorization flow. This endpoint will be used to complete the Authorization process initiated from the fronted UI.

--- backend/app/app/api/api_v1/endpoints/farms/farms.py
+++ backend/app/app/api/api_v1/endpoints/farms/farms.py
@@ -1,14 +1,15 @@
 from typing import List
 
 from fastapi import APIRouter, Body, Depends, HTTPException, Query
-from starlette.requests import Request
-from pydantic import BaseModel
 from sqlalchemy.orm import Session
+import requests
+import time
 
 from app import crud
 from app.api.utils.db import get_db
 from app.api.utils.farms import get_farm_list, get_farm_client, ClientError
 from app.models.farm import Farm, FarmCreate, FarmUpdate
+from app.models.farm_token import FarmTokenCreate, FarmAuthorizationParams
 
 from farmOS import farmOS
 
@@ -93,6 +94,53 @@ async def delete_farm(
     return farm
 
 # /farms/info/ endpoint for accessing farmOS info
+
+@router.put("/{farm_id}/authorize/")
+async def authorize_farm(
+    farm_id: int,
+    *,
+    db: Session = Depends(get_db),
+    auth_params: FarmAuthorizationParams,
+):
+    """
+    Authorize a farm. Complete the OAuth Authorization Flow.
+    """
+    farm = crud.farm.get_by_id(db, farm_id=farm_id)
+
+    data = {}
+    data['code'] = auth_params.code
+    data['state'] = auth_params.state
+    data['grant_type'] = auth_params.grant_type
+    data['client_id'] = auth_params.client_id
+    data['redirect_uri'] = farm.url + "/api/authorized"
+
+    if auth_params.client_secret is not None:
+        data['client_secret'] = auth_params.client_secret
+
+    if auth_params.redirect_uri is not None:
+        data['redirect_uri'] = auth_params.redirect_uri
+
+    token_url = farm.url + "/oauth2/token"
+
+    response = requests.post(token_url, data)
+
+    if response.status_code == 200:
+        response_token = response.json()
+        if "expires_at" not in response_token:
+            response_token['expires_at'] = str(time.time() + int(response_token['expires_in']))
+        new_token = FarmTokenCreate(farm_id=farm.id, **response_token)
+
+        old_token = crud.farm_token.get_farm_token(db, farm_id)
+        if old_token is None:
+            token = crud.farm_token.create_farm_token(db, token=new_token)
+        else:
+            token = crud.farm_token.update_farm_token(db, token=old_token, token_in=new_token)
+
+        return token
+    else:
+        return response.content
+
+
 
 @router.get("/info/", tags=["farm info"])
 def get_all_farm_info(
