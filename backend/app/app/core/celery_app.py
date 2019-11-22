1cf3062afb458f44a115539c97b068066861cf70
Paul Weidner <paul.weidner@gmail.com>
2019-11-22 15:56:34 -0800

    Schedule a Celery worker task to ping farms twice a day. Closes #28.

--- backend/app/app/core/celery_app.py
+++ backend/app/app/core/celery_app.py
@@ -1,5 +1,21 @@
 from celery import Celery
 
+from app.core.config import CELERY_WORKER_PING_INTERVAL
+
 celery_app = Celery("worker", broker="amqp://guest@queue//")
 
-celery_app.conf.task_routes = {"app.worker.test_celery": "main-queue"}
+# Define the default timezone, user for scheduling.
+celery_app.conf.timezone = 'UTC'
+
+celery_app.conf.task_routes = {
+    "app.worker.ping_farms": "main-queue"
+}
+
+# Add tasks to the celery beat schedule.
+celery_app.conf.beat_schedule = {
+    'ping-every-farm': {
+        'task': 'app.worker.ping_farms',
+        'schedule': CELERY_WORKER_PING_INTERVAL,
+    },
+}
+
--- backend/app/app/worker.py
+++ backend/app/app/worker.py
@@ -1,11 +1,30 @@
+import logging
+
 from raven import Client
 
 from app.core import config
 from app.core.celery_app import celery_app
+from app.db.session import Session
+from app import crud
+from app.api.utils.farms import get_farm_client
 
 client_sentry = Client(config.SENTRY_DSN)
 
 
-@celery_app.task(acks_late=True)
-def test_celery(word: str):
-    return f"test task return {word}"
+@celery_app.task()
+def ping_farms():
+    db_session = Session()
+    farm_list = crud.farm.get_multi(db_session)
+
+    total_response = 0
+    for farm in farm_list:
+        try:
+            farm_client = get_farm_client(db_session=db_session, farm=farm)
+            info = farm_client.info()
+            #logging.info("Farm: " + str(farm.id) + " version: " + info['api_version'])
+            total_response += 1
+        except Exception as e:
+            continue
+
+    db_session.close()
+    return f"Pinged {total_response}/{len(farm_list)} farms."
