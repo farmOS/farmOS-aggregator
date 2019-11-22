4294b0a44e704e0bb97371ad69940e30a09e6a3b
Paul Weidner <paul.weidner@gmail.com>
2019-11-22 10:34:58 -0800

    Update all /farm endpoints to use new farm_access dependencies.

--- backend/app/app/api/api_v1/endpoints/farms/areas.py
+++ backend/app/app/api/api_v1/endpoints/farms/areas.py
@@ -6,13 +6,13 @@ from pydantic import BaseModel
 from sqlalchemy.orm import Session
 
 from app.api.utils.db import get_db
-from app.api.utils.farms import get_farm_list, get_farm_client, ClientError
-
-from farmOS import farmOS
+from app.api.utils.farms import get_farms_url_or_list, get_farm_client, ClientError
+from app.models.farm import Farm
 
 router = APIRouter()
 
 # /farms/areas/ endpoint for accessing farmOS areas
+
 
 class Area(BaseModel):
     class Config:
@@ -22,21 +22,20 @@ class Area(BaseModel):
     vocabulary: int
     area_type: str
 
+
 class AreaUpdate(BaseModel):
     class Config:
         extra = 'allow'
 
     id: int
 
+
 @router.get("/")
 def get_all_farm_areas(
     request: Request,
-    farm_id: List[int] = Query(None),
-    farm_url: str = Query(None),
+    farm_list: List[Farm] = Depends(get_farms_url_or_list),
     db: Session = Depends(get_db),
 ):
-    farm_list = get_farm_list(db, farm_id_list=farm_id, farm_url=farm_url)
-
     query_params = {**request.query_params}
     query_params.pop('farm_id', None)
     query_params.pop('farm_url', None)
@@ -59,15 +58,13 @@ def get_all_farm_areas(
 
     return data
 
+
 @router.post("/")
 def create_farm_area(
     area: Area,
-    farm_id: List[int] = Query(None),
-    farm_url: str = Query(None),
+    farm_list: List[Farm] = Depends(get_farms_url_or_list),
     db: Session = Depends(get_db),
 ):
-    farm_list = get_farm_list(db, farm_id_list=farm_id, farm_url=farm_url)
-
     data = {}
     for farm in farm_list:
         data[farm.id] = []
@@ -86,16 +83,14 @@ def create_farm_area(
             continue
 
     return data
+
 
 @router.put("/")
 def update_farm_area(
     area: AreaUpdate,
-    farm_id: List[int] = Query(None),
-    farm_url: str = Query(None),
+    farm_list: List[Farm] = Depends(get_farms_url_or_list),
     db: Session = Depends(get_db),
 ):
-    farm_list = get_farm_list(db, farm_id_list=farm_id, farm_url=farm_url)
-
     data = {}
     for farm in farm_list:
         data[farm.id] = []
@@ -115,15 +110,13 @@ def update_farm_area(
 
     return data
 
+
 @router.delete("/")
 def delete_farm_area(
     id: int,
-    farm_id: List[int] = Query(None),
-    farm_url: str = Query(None),
+    farm_list: List[Farm] = Depends(get_farms_url_or_list),
     db: Session = Depends(get_db),
 ):
-    farm_list = get_farm_list(db, farm_id_list=farm_id, farm_url=farm_url)
-
     data = {}
     for farm in farm_list:
         data[farm.id] = []
