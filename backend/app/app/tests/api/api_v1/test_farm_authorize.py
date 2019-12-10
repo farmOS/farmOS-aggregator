2bbff498c835bb8927f1eefc236830025b16f317
Paul Weidner <paul.weidner@gmail.com>
2019-12-09 17:10:56 -0800

    Create test for utils/farm-auth-link. Closes #50

--- backend/app/app/tests/api/api_v1/test_farm_authorize.py
+++ backend/app/app/tests/api/api_v1/test_farm_authorize.py
@@ -1,9 +1,12 @@
+from urllib.parse import urlparse, parse_qs
+
 import requests
 import pytest
 
 from app.core import config
 from app.models.farm_token import FarmAuthorizationParams
 from app.tests.utils.utils import get_server_api, random_lower_string, get_scope_token_headers
+from app.api.utils.security import _validate_token
 
 
 @pytest.fixture
@@ -49,3 +52,40 @@ def test_farm_authorize_oauth_scope(test_farm):
 
     r = requests.post(f"{server_api}{config.API_V1_STR}/utils/authorize-farm/{test_farm.id}")
     assert r.status_code == 401
+
+
+def test_get_farm_auth_link(test_farm, superuser_token_headers):
+    server_api = get_server_api()
+    server_host = config.SERVER_HOST
+
+    r = requests.post(
+        f"{server_api}{config.API_V1_STR}/utils/farm-auth-link/{test_farm.id}",
+        headers=superuser_token_headers,
+    )
+    assert 200 <= r.status_code < 300
+    assert r.json() is not None
+
+    # Check the returned farm authorization link
+    link = urlparse(r.json())
+    assert link.scheme is not None
+
+    # Cannot assert netloc == server_host because it is defined in an environment variable
+    # that is not set in the backend-tests container
+    # assert link.netloc == server_host
+    assert link.netloc is not ''
+
+    # Check that the path includes the correct farm ID
+    assert link.path == f"/authorize-farm/{test_farm.id}"
+
+    # Check that an api_token query param is included
+    assert link.query is not None
+    params = parse_qs(link.query)
+    assert 'api_token' in params
+    token = params['api_token'][0]
+
+    # Validate the api_token
+    token_data = _validate_token(token)
+    assert token_data is not None
+    assert token_data.farm_id == [test_farm.id]
+    assert token_data.scopes is not None
+    assert len(token_data.scopes) > 0
