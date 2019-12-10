9856ff87089597d8062e701f337b6bef39c9768b
Paul Weidner <paul.weidner@gmail.com>
2019-12-09 17:19:59 -0800

    Create unit test for api_token. Closes #52

---
+++ backend/app/app/tests/crud/test_api_token.py
@@ -0,0 +1,20 @@
+from app.core.jwt import create_farm_api_token
+from app.api.utils.security import _validate_token
+
+
+def test_create_api_token():
+    farm_id_list = [1, 2, 3]
+    scopes = ["scope1", "scope2"]
+
+    token = create_farm_api_token(farm_id_list, scopes)
+    assert token is not None
+
+    token_data = _validate_token(token)
+    assert token_data is not None
+
+    # api_tokens are not associated with a user.
+    assert token_data.user_id is None
+
+    # Check that farm_id and scopes match.
+    assert token_data.farm_id == farm_id_list
+    assert token_data.scopes == scopes
