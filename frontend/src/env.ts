d04fc8b9dbb37b8c635a8f3b017c5362d149cce9
paul121 <paul.weidner@gmail.com>
2020-03-02 14:33:43 -0800

    Default to the window.location.origin for apiUrl after reloading a page.

--- frontend/src/env.ts
+++ frontend/src/env.ts
@@ -5,5 +5,11 @@ declare global {
 window._env = window._env || {};
 
 export function env(key = '') {
+  // Reloading a page may make the Vue app make an API
+  // before knowing the apiURL that is loaded in with a JS file.
+  // This allows the window origin to be used in these cases.
+  if (window._env[key] === undefined && key === 'apiUrl') {
+    return window.location.origin;
+  }
   return window._env[key];
 }
