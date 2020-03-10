f5d21582e27ea5515a95242b6eb3f6437ccf52a1
Paul Weidner <paul.weidner@gmail.com>
2020-03-10 16:47:10 -0700

    Create API Keys as JWT with no expiration.

--- backend/app/app/core/jwt.py
+++ backend/app/app/core/jwt.py
@@ -19,6 +19,21 @@ def create_access_token(*, data: dict, expires_delta: timedelta = None):
     return encoded_jwt
 
 
+def create_api_key(farm_id: List[int], scopes: List[str], all_farms=False):
+    now = datetime.utcnow()
+    encoded_jwt = jwt.encode(
+        {
+            'nbf': now.timestamp(),
+            'farm_id': farm_id,
+            'all_farms': all_farms,
+            'scopes': scopes,
+        },
+        settings.SECRET_KEY,
+        algorithm=ALGORITHM,
+    )
+    return encoded_jwt
+
+
 def create_farm_api_token(farm_id: List[int], scopes: List[str]):
     delta = timedelta(hours=settings.EMAIL_RESET_TOKEN_EXPIRE_HOURS)
     now = datetime.utcnow()
