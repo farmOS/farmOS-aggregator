5359d7a32678e5b0f3d6134a9f084e5ec66158a8
Paul Weidner <paul.weidner@gmail.com>
2020-03-10 16:41:29 -0700

    Create api-keys endpoint.

--- backend/app/app/api/api_v1/api.py
+++ backend/app/app/api/api_v1/api.py
@@ -2,9 +2,9 @@ import logging
 
 from fastapi import APIRouter, Depends, Security
 
-from app.api.api_v1.endpoints import login, users, utils
+from app.api.api_v1.endpoints import login, users, utils, api_key
 from app.api.api_v1.endpoints.farms import farms, info, logs, assets, terms, areas
-from app.api.utils.security import get_farm_access
+from app.api.utils.security import get_farm_access, get_current_active_superuser
 
 logger = logging.getLogger(__name__)
 
@@ -12,6 +12,14 @@ api_router = APIRouter()
 api_router.include_router(login.router, tags=["login"])
 api_router.include_router(users.router, prefix="/users", tags=["users"])
 api_router.include_router(utils.router, prefix="/utils", tags=["utils"])
+
+# Include /api-keys endpoint, require superuser to access.
+api_router.include_router(
+    api_key.router,
+    prefix="/api-keys",
+    tags=["api keys"],
+    dependencies=[Security(get_current_active_superuser)]
+)
 
 # Include /farms endpoints.
 api_router.include_router(
---
+++ backend/app/app/api/api_v1/endpoints/api_key.py
@@ -0,0 +1,72 @@
+import logging
+from typing import List
+from datetime import timedelta
+
+from fastapi import APIRouter, Body, Depends, HTTPException
+from sqlalchemy.orm import Session
+
+from app import crud
+from app.api.utils.db import get_db
+from app.schemas.api_key import ApiKey, ApiKeyCreate, ApiKeyUpdate
+
+logger = logging.getLogger(__name__)
+
+router = APIRouter()
+
+
+@router.get("/", response_model=List[ApiKey])
+def get_api_keys(db: Session = Depends(get_db)):
+    """
+    Return all api keys.
+    """
+    return crud.api_key.get_multi(db)
+
+
+@router.post("/", response_model=ApiKey)
+def create_api_key(
+    *,
+    db: Session = Depends(get_db),
+    key_in: ApiKeyCreate,
+):
+    """
+    Create a new API Key.
+    """
+    api_key = crud.api_key.create(db, api_key_in=key_in)
+
+    return api_key
+
+
+@router.put("/{key_id}", response_model=ApiKey)
+def update_api_key(
+    *,
+    db: Session = Depends(get_db),
+    key_id: int,
+    key_in: ApiKeyUpdate,
+):
+    """
+    Update an existing API Key.
+
+    Only the name, notes, and enabled status can be updated.
+    """
+    key = crud.api_key.get_by_id(db, key_id=key_id)
+    if not key:
+        raise HTTPException(
+            status_code=404,
+            detail="API Key not found.",
+        )
+
+    key = crud.api_key.update(db, api_key=key, api_key_in=key_in)
+    return key
+
+
+@router.delete("/{key_id}")
+def delete_api_key(
+    *,
+    db: Session = Depends(get_db),
+    key_id: int
+):
+    """
+    Delete an API Key.
+    """
+    key = crud.api_key.delete(db, key_id=key_id)
+    return key
