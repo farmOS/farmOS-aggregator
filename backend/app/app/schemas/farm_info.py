c2cf94ae9892092510a771c03e95a41f1ebe4897
paul121 <paul.weidner@gmail.com>
2021-01-21 13:41:27 -0800

    Alias api_version as version for 2.x servers.

--- backend/app/app/schemas/farm_info.py
+++ backend/app/app/schemas/farm_info.py
@@ -1,13 +1,13 @@
 from typing import Optional
 
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 
 # farmOS Server Info to cache
 class FarmInfo(BaseModel):
     name: str
     url: str
-    api_version: str
+    api_version: str = Field(alias="version")
     user: Optional[dict]
     system_of_measurement: Optional[str]
     resources: Optional[dict]
