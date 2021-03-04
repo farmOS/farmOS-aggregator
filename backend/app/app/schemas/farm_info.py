372dfc73df501497f2f60d8c88fa742e860599e0
paul121 <paul.weidner@gmail.com>
2021-03-04 10:54:58 -0800

    Alias the FarmInfo.version field with "api_version" so that value is always returned.

--- backend/app/app/schemas/farm_info.py
+++ backend/app/app/schemas/farm_info.py
@@ -5,9 +5,12 @@ from pydantic import BaseModel, Field
 
 # farmOS Server Info to cache
 class FarmInfo(BaseModel):
+    class Config:
+        allow_population_by_field_name = True
+
     name: str
     url: str
-    api_version: str = Field(alias="version")
+    version: str = Field(alias="api_version")
     user: Optional[dict]
     system_of_measurement: Optional[str]
     resources: Optional[dict]
